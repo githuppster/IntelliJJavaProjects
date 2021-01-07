708f80ebc89f286b96655b1fb0fa4e089f79cae0
hhuppster <hhuppster@gmail.com>
2021-01-07 15:43:09 +0000

    first java hhuppster repo for github - modified ver 1

---
+++ src/com/amigoscode/Main.java
@@ -0,0 +1,11 @@
+package com.amigoscode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i ++) {
+            System.out.println("eins " + i);
+            System.out.println("zwei " + 2 * i);
+        }
+    }
+}
